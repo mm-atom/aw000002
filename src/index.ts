1e588d85a4b0acb26cf978cecdc93717ec48049e
taoqf <tao_qf@163.com>
2020-03-07 14:32:42 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export default function fun() {
+export default interface IAiBrowserPage {
+	readonly data: {
+		readonly [attr: string]: unknown;
+		readonly params: { [key: string]: unknown };
+		readonly url: string;
+	};
+	fire<T>(action: string, ...args: unknown[]): Promise<T>;
+	emit<T>(event: string, ...args: unknown[]): Promise<T>;
+	// eslint-disable-next-line semi
 }
